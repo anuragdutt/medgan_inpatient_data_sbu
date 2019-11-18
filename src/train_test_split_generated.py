eeed119a4f0adf2d46a7348bfa18b23623abad26
anuragdutt <anurag2709@gmail.com>
2019-11-18 10:07:36 -0500

    Adding train test split

---
+++ src/train_test_split_generated.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def convertTo3DigitIcd9(dxStr):
+	return str(dxStr)[:3]	
+
+
+def trainTestSplit(adm_file, diag_file, t_size = 0.2):
+	adm = pd.read_csv(adm_file)
+	dia = pd.read_csv(diag_file)
+	dat = pd.merge(adm, dia, on = ["pid", "visit_id"], how = "inner")
+	train, test = train_test_split(dat, test_size = t_size)
+
+	adm_train = train.loc[:,['pid', 'visit_id', 'visit_start', 'visit_end']]
+	dia_train = train.loc[:, ['pid', 'visit_id', 'seq', 'icd_codes']]
+
+	adm_test = test.loc[:,['pid', 'visit_id', 'visit_start', 'visit_end']]
+	dia_test = test.loc[:, ['pid', 'visit_id', 'seq', 'icd_codes']]
+
+	return adm_train, dia_train, adm_test, dia_test
+
+
+if __name__ == "__main__":
+
+	# loading the binary dataset
+	adm_chk = pd.read_csv("../generated/ADMISSIONS_GENERATED_NOMERGE.csv")
+	dia_chk = pd.read_csv("../generated/DIAGNOSES_ICD_GENERATED_NOMERGE.csv")
+	chk = pd.merge(adm_chk, dia_chk, on = ["pid", "visit_id"], how = "outer")
+	chk['icd_3digit'] = chk['icd_codes'].map(convertTo3DigitIcd9)
+	print(len(chk['icd_3digit'].unique()))
+
+
+
+	adm = pd.read_csv("../generated/ADMISSIONS_GENERATED.csv")
+	dia = pd.read_csv("../generated/DIAGNOSES_ICD_GENERATED.csv")
+
+	orig = pd.merge(adm, dia, on = ["pid", "visit_id"], how = "inner")
+	orig['icd_3digit'] = orig['icd_codes'].map(convertTo3DigitIcd9)
+	print(len(orig['icd_3digit'].unique()))
+
+	t_size = 0.2
+
+	af = "../generated/ADMISSIONS_GENERATED.csv"
+	df = "../generated/DIAGNOSES_ICD_GENERATED.csv"
+	adm_train, dia_train, adm_test, dia_test = trainTestSplit(af, df, t_size = t_size)
+	
+	adm_train.to_csv("../generated/ADMISSIONS_GENERATED_TRAIN.csv")
+	dia_train.to_csv("../generated/DIAGNOSES_ICD_GENERATED_TRAIN.csv")
+	adm_test.to_csv("../generated/ADMISSIONS_GENERATED_TRAIN_TEST.csv")
+	dia_test.to_csv("../generated/DIAGNOSES_ICD_GENERATED_TEST.csv")
+
+
+
+	# icd_headers_binary_sumstats = np.load("../pretrain/binary.types", allow_pickle = True)
+	# icd_headers_binary = [i for i in icd_headers_binary_sumstats.keys()]
+	# # print(len(icd_headers_binary))
+	# print(icd_headers_binary)
+
+
+	# # raw = np.load("../synthetic/binary/cerner_synthetic.npy")
+	# # print(raw.shape)
+	# # print(raw[10])
+
+	# ## Analysis for count
+	# raw = np.load("../synthetic/count/cerner_synthetic.npy")
+	# print(raw.shape)
+
+	
+	# print(raw[0])
