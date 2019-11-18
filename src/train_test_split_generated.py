9f1e1d111256c745c1119ce65ad03668c6a8f502
anuragdutt <anurag2709@gmail.com>
2019-11-18 14:38:24 -0500

    Fixing blank visit ids issue

--- src/train_test_split_generated.py
+++ src/train_test_split_generated.py
@@ -44,9 +44,9 @@ def trainTestSplit(adm_file, diag_file, t_size = 0.2):
 	dia_test = dia_test.drop(['icd_3digit'], axis = 1)
 
 
-	print(adm_train[adm_train['visit_id'].isin(['2014'])])
-	print(dia[dia['visit_id'].isin(['2014'])])
-	exit(0)
+	# print(adm_train[adm_train['visit_id'].isin(['2014'])])
+	# print(dia[dia['visit_id'].isin(['2014'])])
+	# exit(0)
 
 	# print(adm_train.shape)
 	# print(adm_test.shape)
